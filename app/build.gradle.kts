4cc36796bc4bfa35e71950c05605f25eb0f42b01
digitalby <yury@digitalby.me>
2026-03-29 23:14:32 +0200

    Add GitHub build action

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,10 +19,23 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
+  signingConfigs {
+    val keystorePath = System.getenv("KEYSTORE_PATH")
+    if (keystorePath != null) {
+      create("release") {
+        storeFile = file(keystorePath)
+        storePassword = System.getenv("STORE_PASSWORD")
+        keyAlias = System.getenv("KEY_ALIAS")
+        keyPassword = System.getenv("KEY_PASSWORD")
+      }
+    }
+  }
+
   buildTypes {
     release {
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+      signingConfig = signingConfigs.findByName("release")
     }
   }
   compileOptions {
