967c69883af837ad52a19b77a791eba793636f5e
digitalby <yury@digitalby.me>
2026-04-06 19:07:31 +0200

    chore: use semver versionName (1.1.0) to match CI tag pattern

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     minSdk = 23
     targetSdk = 36
     versionCode = 2
-    versionName = "1.1"
+    versionName = "1.1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
