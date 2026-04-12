80f5d9e05860a09f5f20864d073794e5aaf75884
digitalby <yury@digitalby.me>
2026-04-12 17:36:06 +0200

    fix: drop AGP dependency info signing block so F-Droid scanner accepts the release APK (v1.1.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "info.yuryv.androiddebugmode"
     minSdk = 23
     targetSdk = 36
-    versionCode = 2
-    versionName = "1.1.0"
+    versionCode = 3
+    versionName = "1.1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
